8e9388ca16d27be67e1b4840d10f700634fa05e5
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-08-23 17:22:39 -0500

    Cleanup code

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -21,7 +21,6 @@ function Review() {
             method: 'POST',
             url: '/feedback',
             data: { feeling, understanding, support, comments}
-            //data: feedbackObject
         }).then((response) => {
             console.log(response);
         }).catch((error)=> {
@@ -35,28 +34,27 @@ function Review() {
         <Container maxWidth="sm">
             <Paper elevation={0} className="formContainer">
                 
+                <h1>Review</h1>
+                <h3>Feelings: {feeling}</h3>
+                <h3>Understanding: {understanding}</h3>
+                <h3>Support: {support}</h3>
+                <h3>Comments: {comments}</h3>
+                    
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={handlePrev}
+                >
+                    Previous Page
+                </Button>
 
-        <h1>Review</h1>
-            <h3>Feelings: {feeling}</h3>
-            <h3>Understanding: {understanding}</h3>
-            <h3>Support: {support}</h3>
-            <h3>Comments: {comments}</h3>
-                
-        <Button
-            variant="contained"
-            color="secondary"
-            onClick={handlePrev}
-        >
-            Previous Page
-        </Button>
-
-        <Button
-            variant="contained"
-            color="primary"
-            onClick={handleSubmit}
-        >
-            Next Page
-        </Button>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSubmit}
+                >
+                    Next Page
+                </Button>
             </Paper>
         </Container>
     )
